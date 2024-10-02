8293928c386692f7e358cf33628515a089293d35
jahidhasan6676 <jh18186676@gmail.com>
2024-10-02 16:00:33 +0600

    added the practice file

---
+++ practice-task.js
@@ -0,0 +1,36 @@
+// task-1
+
+// setTimeout(() => {
+//     console.log('hello chando')
+// }, 5000)
+
+// setTimeout(() => {
+//     console.log('Alice')
+// }, 2000);
+// console.log('Hello')
+
+// task-2
+
+// function tellJoke(){
+//     console.log('My name is jahid khan, ami  sobaike kore dei khan khan')
+// }
+// let num = 0;
+// const jokeInterval = setInterval(() => {
+//     tellJoke();
+//     num++;
+    
+//     console.log(jokeInterval, num)
+// }, 2000)
+
+// setTimeout(() => {
+    
+//     clearTimeout(jokeInterval)
+// }, 10000)
+
+// task-3
+
+// async function usersData(){
+//     const res = await fetch('https://v2.jokeapi.dev/joke/Programming?type=single');
+//     const data = await res.json();
+//     console.log(data);
+// }
